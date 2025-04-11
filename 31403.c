b104bfd6306e6684d1a96550e5d64906b8ac624d
Bctp-7603 <rudals3620@naver.com>
2025-04-11 11:58:32 +0000

    2025-04-11_1

--- 31403.c
+++ 31403.c
@@ -1,34 +1,67 @@
+// itoa() 는 표준 함수가 아님. <stdlib.h>에 정의가 안되어 있을 수 있음.
+
+
 #include <stdio.h>
-#include <string.h> // strcat()
-#include <ctype.h> // atoi()
+#include <string.h>
+#include <stdlib.h>
+
+void reverse(char s[])
+ {
+     int i, j;
+     char c;
+ 
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+ }
+
+void itoa(int n, char s[])
+ {
+     int i, sign;
+ 
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+ }
 
 int main() {
     int a, b, c;
     char A[5], B[5], C[5];
-    
+
+    scanf("%d", &a);
     while(1) {
         if(a >= 1 && a <= 1000) {
-            scanf("%d", &a);
             break;
         }
         else return 0;
     }
+    scanf("%d", &b);
     while(1) {
         if(a >= 1 && a <= 1000) {
-            scanf("%d", &b);
             break;
         }
         else return 0;
     }
+    scanf("%d", &c);
     while(1) {
         if(a >= 1 && a <= 1000) {
-            scanf("%d", &c);
             break;
         }
         else return 0;
     }
 
-    
+    itoa(a, A);
+    itoa(b, B);
+    itoa(c, C);
 
     printf("%d\n", a+b-c);
     strcat(A,B);
