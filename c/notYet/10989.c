e3905dc03385ae974d74de8f823ebca48d0d5f30
Bctp-7603 <rudals3620@naver.com>
2025-04-23 14:35:05 +0000

    2025-04-23

--- c/notYet/10989.c
+++ c/notYet/10989.c
@@ -5,36 +5,29 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-int compare(const void* arg1, const void* arg2) {
-	int a = *(int*)arg1;    //void를 int형으로 변환
-	int b = *(int*)arg2;
-	//오름차순 정렬
-	if (a > b) return 1;
-	else if (a < b) return -1;
-	else return 0;
-}
 
 int main() {
     int count;
 
     scanf("%d", &count);
 
-    int* num = (int*)malloc(sizeof(int) * count);
+    int num[10001];
 
-    for(int i = 0; i < count; i++) {
-        scanf("%d", &num[i]);
-        if(num[i] < -1000000 || num[i] > 1000000) return 0;
+    for(int i = 0; i < 10001; i++) {
+        num[i] = 0;
     }
 
-    qsort(num, count, sizeof(int), compare);
-
     for(int i = 0; i < count; i++) {
-        printf("%d\n", num[i]);
+        int input;
+        scanf("%d", &input);
+        num[input]++;
     }
 
-    free(num);
+    for(int i = 0; i < 10001; i++) {
+        for(int j = 0; j < num[i]; j++) {
+            printf("%d\n", i);
+        }
+    }
 
     return 0;
 }
